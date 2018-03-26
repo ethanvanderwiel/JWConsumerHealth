49bbab32a09007e0f23f0e042a706d6af6bf41a0
Chris Davenport <chris@christopherdavenport.tech>
2018-03-26 16:32:28 -0400

    Remove Postgres Dynamic

--- src/main/scala/com/banno/template/config/ConfigService.scala
+++ src/main/scala/com/banno/template/config/ConfigService.scala
@@ -2,13 +2,13 @@ package com.banno.template.config
 
 import cats.effect._
 import fs2._
-import com.banno.template.migrations.Migrations
+// import com.banno.template.migrations.Migrations
 import com.banno.zookeeper.Zookeeper
 import com.banno.zookeeper.{ServiceDiscovery => SD}
 import com.banno.zookeeper.tagless.ServiceDiscovery
 import doobie.util.transactor.Transactor
 import java.net.InetAddress
-import scala.concurrent.ExecutionContext
+// import scala.concurrent.ExecutionContext
 import org.http4s.client.Client
 import org.http4s.client.blaze.Http1Client
 import org.http4s.server.blaze.BlazeBuilder
@@ -25,7 +25,7 @@ trait ConfigService[F[_]] {
 
 object ConfigService {
 
-  def impl[F[_]](implicit Effect: Effect[F], S: Scheduler, ec: ExecutionContext): Stream[F, ConfigService[F]] =
+  def impl[F[_]](implicit Effect: Effect[F]/*, S: Scheduler, ec: ExecutionContext*/): Stream[F, ConfigService[F]] =
     for {
       initConfig <- Stream.eval(SetupConfig.loadConfig[F])
       client <- Http1Client.stream[F]()
@@ -33,16 +33,20 @@ object ConfigService {
       hostname <- Stream.eval(Sync[F].delay{
         sys.env.get("HOST").orElse(sys.env.get("HOSTNAME")).getOrElse(InetAddress.getLocalHost.getHostName)
       })
-      updatedDbConfig <- SetupConfig.loadPostgresConfig[F](initConfig.postgres, initConfig.vault)(Effect, client, S, ec)
+      // Uncomment the Below To Fetch Dynamic Credentials
+      // updatedDbConfig <- SetupConfig.loadPostgresConfig[F](initConfig.postgres, initConfig.vault)(Effect, client, S, ec)
     } yield
       new ConfigService[F] {
+
         override def primaryHttpServer: BlazeBuilder[F] =
           BlazeBuilder[F](Effect).bindHttp(initConfig.http.port, "0.0.0.0")
         override def administrativeHttpServer: BlazeBuilder[F] =
           BlazeBuilder[F](Effect).bindHttp(initConfig.health.port, "0.0.0.0")
         override def httpClient: Client[F] = client
         override def serviceDiscovery: Stream[F, ServiceDiscovery[F]] = ServiceDiscovery.fromCuratorFramework(curator)
-        override def transactor: Stream[F, Transactor[F]] = SetupConfig.loadConfigTransactor[F](updatedDbConfig)
+        // Uncomment Below to Load Transactor Correctly
+        // override def transactor: Stream[F, Transactor[F]] = SetupConfig.loadConfigTransactor[F](updatedDbConfig)
+        override def transactor: Stream[F, Transactor[F]] = ??? 
         override def registerInstance: Stream[F, Unit] = SD.registerInstanceStreamed(
           curator,
           initConfig.registration.path,
@@ -54,8 +58,10 @@ object ConfigService {
           hostname,
           ""
         )
-        override def runMigrations: F[Unit] =
-          Migrations.makeMigrations[F](updatedDbConfig.jdbcUrl, updatedDbConfig.username, updatedDbConfig.password)
+        // Uncomment Below to Enable Running Migrations
+        // override def runMigrations: F[Unit] =
+        //   Migrations.makeMigrations[F](updatedDbConfig.jdbcUrl, updatedDbConfig.username, updatedDbConfig.password)
+        override def runMigrations: F[Unit] = ???
       }
 
 }
